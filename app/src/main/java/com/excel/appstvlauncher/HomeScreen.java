f1cbb9ee6cba689350c5f06fa3d0aecc67cc9552
Sohail Shaikh <unknown0492@gmail.com>
2019-03-23 02:45:13 +0530

    Initial Commit This is an Empty Launcher having package name com.excel.appstvlauncher. This is for the use with 2nd gen apks in GIEC box for PEH firmware. The GIEC has written a code whereby, power on/off button on the remote will call the .HomeScreen from the Launcher package, which is not possible in 2nd gen because the package name is different. So, we had to create this apk so that when power on/off is pressed, this apk will invoke which has code to go back to Home Screen

---
+++ app/src/main/java/com/excel/appstvlauncher/HomeScreen.java
@@ -0,0 +1,21 @@
+package com.excel.appstvlauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.excel.excelclasslibrary.UtilShell;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d( "asshole", "aaaa " );
+        UtilShell.executeNonSUShellCommandWithOp( "input keyevent 3" );
+        //UtilMisc.startApplicationUsingPackageName( this, "com.excel.appstvlauncher.secondgen" );
+        finish();
+    }
+}
